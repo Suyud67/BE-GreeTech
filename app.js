8cf74540305d47846f1af6a6a2c47068eb622846
suyud67 <suyudsetiawan@gmail.com>
2022-12-04 22:30:38 +0800

    fix CORS config

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 // import routes and db
 const routes = require('./routes/routes');
 require('./config/db');
+
+// config cors in express
+routes.use(cors());
 
 // use routes
 // give public access for folder image upload
